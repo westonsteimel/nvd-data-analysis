80a6aafb00dc6419bb7d06e92c74a49344f588e0
Weston Steimel <weston.steimel@gmail.com>
2021-03-05 07:56:24 +0000

    ensure we actually capture all of the cpes from the match feed and store a file with all cpes
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- cpe.py
+++ cpe.py
@@ -9,37 +9,45 @@ CPE_DICT_V23_URL = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-d
 
 cpe_matches = json.loads(gzip.decompress(requests.get(CPE_MATCH_URL).content))["matches"]
 
-cpes_with_no_name = []
+cpes_with_no_official_entry = []
+match_feed_cpes = {}
 all_cpes = {}
 
 for m in cpe_matches:
     cpe_uri = m.get("cpe23Uri")
 
     if not m.get("cpe_name"):
-        cpes_with_no_name.append(cpe_uri)
+        cpes_with_no_official_entry.append(cpe_uri)
+
+    for cpe in m.get("cpe_name", []):
+        uri = m.get("cpe23Uri")
+
+        if uri:
+            all_cpes[uri] = True
+            match_feed_cpes[uri] = True
 
     all_cpes[cpe_uri] = True
+    match_feed_cpes[cpe_uri] = True
 
-cpes_with_no_name = sorted(cpes_with_no_name)
+cpes_with_no_official_entry = sorted(cpes_with_no_official_entry)
 
 official_cpe_list = xmltodict.parse(gzip.decompress(requests.get(CPE_DICT_V23_URL).content))['cpe-list']['cpe-item']
 
 cpe_23_dict = {}
 
 for item in official_cpe_list:
-    if item.get("@deprecated", False) == "true":
-        continue
-
+    deprecated = item.get("@deprecated", False) == "true"
     cpe23 = item.get('cpe-23:cpe23-item')
 
     if cpe23:
         name = cpe23.get('@name')
-        cpe_23_dict[name] = item
-        all_cpes[name] = True
-
-    else:
-        name = item.get('@name')
-        cpe_22_dict[name] = item
+        
+        if not deprecated:
+            cpe_23_dict[name] = item
+            all_cpes[name] = True
+        else:
+            if name in all_cpes:
+                del all_cpes[name]
 
 cpe_prefixes = {}
 
@@ -50,16 +58,21 @@ for cpe in all_cpes.keys():
     cpe_prefixes[prefix] = True
 
 sorted_prefixes = sorted(cpe_prefixes.keys())
+sorted_all = sorted(all_cpes.keys())
 
 os.makedirs('data/cpe/', exist_ok=True)
 
-print(f'CPE match feed total: {len(cpe_matches)}')
-print(f'Missing official entry: {len(cpes_with_no_name)}')
-print(f'Official Entries Total: {len(official_cpe_list)}')
+print(f'CPE match feed total: {len(match_feed_cpes)}')
+print(f'Official Entries Total: {len(cpe_23_dict)}')
+print(f'Missing official entry: {len(cpes_with_no_official_entry)}')
+print(f'All: {len(sorted_all)}')
 print(f'Unique CPE prefixes: {len(sorted_prefixes)}')
 
-with open('data/cpe/missing_cpe_dict_entries.json', 'w+') as f:
-    json.dump(cpes_with_no_name, f, indent=2)
+with open('data/cpe/cpes_with_no_official_entry.json', 'w+') as f:
+    json.dump(cpes_with_no_official_entry, f, indent=2)
+
+with open('data/cpe/all_cpes.json', 'w+') as f:
+    json.dump(sorted_all, f, indent=2)
 
 with open('data/cpe/unique_prefixes.json', 'w+') as f:
     json.dump(sorted_prefixes, f, indent=2)
