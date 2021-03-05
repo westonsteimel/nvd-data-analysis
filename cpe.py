a0b45d12bf741a87fceaa07a4818b3e07f8a6c9c
Weston Steimel <weston.steimel@gmail.com>
2021-03-05 07:29:10 +0000

    move the mistaken no match stuff
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- cpe.py
+++ cpe.py
@@ -10,7 +10,6 @@ CPE_DICT_V23_URL = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-d
 cpe_matches = json.loads(gzip.decompress(requests.get(CPE_MATCH_URL).content))["matches"]
 
 cpes_with_no_name = []
-cpes_with_cves = {}
 all_cpes = {}
 
 for m in cpe_matches:
@@ -19,17 +18,13 @@ for m in cpe_matches:
     if not m.get("cpe_name"):
         cpes_with_no_name.append(cpe_uri)
 
-    cpes_with_cves[cpe_uri] = True
     all_cpes[cpe_uri] = True
 
 cpes_with_no_name = sorted(cpes_with_no_name)
 
 official_cpe_list = xmltodict.parse(gzip.decompress(requests.get(CPE_DICT_V23_URL).content))['cpe-list']['cpe-item']
 
-cpe_22_dict = {}
 cpe_23_dict = {}
-
-official_but_no_cve = []
 
 for item in official_cpe_list:
     if item.get("@deprecated", False) == "true":
@@ -42,13 +37,10 @@ for item in official_cpe_list:
         cpe_23_dict[name] = item
         all_cpes[name] = True
 
-        if not cpes_with_cves.get(name):
-            official_but_no_cve.append(name)
     else:
         name = item.get('@name')
         cpe_22_dict[name] = item
 
-official_but_no_cve = sorted(official_but_no_cve)
 cpe_prefixes = {}
 
 for cpe in all_cpes.keys():
@@ -61,17 +53,13 @@ sorted_prefixes = sorted(cpe_prefixes.keys())
 
 os.makedirs('data/cpe/', exist_ok=True)
 
-print(f'Match feed total: {len(cpe_matches)}')
+print(f'CPE match feed total: {len(cpe_matches)}')
 print(f'Missing official entry: {len(cpes_with_no_name)}')
-print(f'Official Entry Total: {len(official_cpe_list)}')
-print(f'Official Entry without CVE: {len(official_but_no_cve)}')
+print(f'Official Entries Total: {len(official_cpe_list)}')
 print(f'Unique CPE prefixes: {len(sorted_prefixes)}')
 
 with open('data/cpe/missing_cpe_dict_entries.json', 'w+') as f:
     json.dump(cpes_with_no_name, f, indent=2)
-
-with open('data/cpe/official_entries_without_cve.json', 'w+') as f:
-    json.dump(official_but_no_cve, f, indent=2)
 
 with open('data/cpe/unique_prefixes.json', 'w+') as f:
     json.dump(sorted_prefixes, f, indent=2)
