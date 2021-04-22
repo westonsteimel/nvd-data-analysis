8ddf129ddbb96ae246008591b3c90137b711a5c8
Weston Steimel <weston.steimel@gmail.com>
2021-04-23 00:01:24 +0100

    updates to the ghsa script to compare with current and allow filtering based on severity
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- ghsa.py
+++ ghsa.py
@@ -4,28 +4,71 @@ import glob
 import requests
 import os
 import os.path
+import copy
 
-found = {}
+current_package_metadata = {}
+updated_package_metadata = {}
 data_path = '/home/weston/github/westonsteimel/vuln-list-main'
+severity='critical'
+advisory_files = glob.glob(f'{data_path}/ghsa/**/*.json', recursive=True)
+package_metadata_files = glob.glob(f'/home/weston/github/westonsteimel/package-metadata/**/*.toml')
 
-files = glob.glob(f'{data_path}/ghsa/**/*.json', recursive=True)
+for metadata_file in package_metadata_files:
+    with open(metadata_file, 'r+') as f:
+        print(metadata_file)
+        metadata = toml.load(f)
+        ecosystem = metadata['ecosystem'].lower()
+        name = metadata['name'].lower()
+        lookup_key = f'{ecosystem}:{name}'
+        current_package_metadata[lookup_key] = metadata
 
-for f in files:
-    components = f.replace(f'{data_path}/', '').split('/')
-    ecosystem = components[1]
-    package = '/'.join(components[2:-1])
+for advisory_file in advisory_files:
+    with open(advisory_file, 'r+') as f:
+        print(advisory_file)
+        advisory = json.load(f)
 
-    if ecosystem not in found:
-        found[ecosystem] = {}
+        if severity:
+            if advisory.get('Severity').lower() != severity:
+                continue
 
-    if package not in found[ecosystem]:
-        with open(f, 'r+') as advisory_file:
-            ghsa = json.load(advisory_file)
-            
-            for ref in ghsa.get('Advisory', {}).get('References', []):
-                if package in found[ecosystem]:
-                    break
+            package_info = advisory.get('Package')
 
+            if not package_info:
+                continue
+
+            ecosystem = package_info['Ecosystem'].lower()
+
+            if ecosystem == 'pip':
+                ecosystem = 'pypi'
+
+            name = package_info['Name']
+            lookup_key = f'{ecosystem}:{name.lower()}'
+
+            if lookup_key not in updated_package_metadata:
+                if lookup_key in current_package_metadata:
+                    package_metadata = copy.deepcopy(current_package_metadata[lookup_key])
+                    assert package_metadata == current_package_metadata[lookup_key]
+
+                else:
+                    package_metadata = {
+                        'name': name,
+                        'ecosystem': ecosystem,
+                        'cpe_configurations': []
+                    }
+
+                package_metadata['cpe_mapping'] = {}
+            else:
+                package_metadata = updated_package_metadata[lookup_key]
+
+            for cpe_config in package_metadata.get('cpe_configurations', []):
+                part = cpe_config.get('part', 'a')
+                vendor = cpe_config.get('vendor')
+                product = cpe_config.get('product')
+                target_software = cpe_config.get('target_software', '*')
+                cpe_key = f'{part}:{vendor}:{product}:{target_software}'
+                package_metadata['cpe_mapping'][cpe_key] = cpe_config
+
+            for ref in advisory.get('Advisory', {}).get('References', []):
                 url = ref.get('Url')
 
                 if url.startswith('https://nvd.nist.gov/vuln/detail/'):
@@ -33,7 +76,7 @@ for f in files:
                     cve_components = cve.split('-')
 
                     if len(cve_components) != 3:
-                        print(cve)
+                        print(f'CVE {cve} in file {advisory_file} had more than 3 components.')
                         continue
 
                     cve_dir = cve_components[1]
@@ -43,11 +86,7 @@ for f in files:
                         with open(cve_path, 'r+') as cve_file:
                             cve_info = json.load(cve_file)
                             nodes = cve_info.get('configurations', {}).get('nodes', [])
-                            package_info = []
-                            found_exact_match = False
 
-                            #TODO: We should try to find the best matching CPE here - for instance does the product
-                            # component contain the package name, etc
                             for node in nodes:
                                 matches = node.get('cpe_match', [])
 
@@ -61,81 +100,63 @@ for f in files:
                                         product = cpe_components[4]
                                         target_software = cpe_components[10]
 
-                                        if product.lower() == package.lower():
-                                            package_info = [{
-                                                'part': part,
+                                        if (part == 'o' and vendor == 'fedoraproject' and product == 'fedora') \
+                                            or (part == 'o' and vendor == 'debian' and product == 'debian_linux'):
+                                            continue
+
+                                        cpe_key = f'{part}:{vendor}:{product}:{target_software}'
+
+                                        if cpe_key not in package_metadata['cpe_mapping']:
+                                            cpe = {
                                                 'vendor': vendor,
-                                                'product': product,
-                                                'target_software': target_software
-                                            }]
-                                            found_exact_match = True
-                                            break
-                                        elif vendor not in ['fedoraproject', 'debian', 'opensuse', 'redhat']:
-                                            package_info.append({
-                                                'part': part,
-                                                'vendor': vendor,
-                                                'product': product,
-                                                'target_software': target_software
-                                            })
-                                
-                                if found_exact_match:
-                                    break
+                                                'product': product, 
+                                            }
 
-                            if package_info:
-                                found[ecosystem][package] = package_info
+                                            if part != 'a':
+                                                cpe['part'] = part
 
-# There are definitely extra CPE's for several microsoft ones 
-# (.net_core, .net_framework, asp.net_core, and usually only one applies to the package)
-# so definitely review any of those
-if 'nuget' in found:
-    for k, v in found['nuget'].items():
-        for i in v:
-            if i.get('vendor')=='microsoft':
-                print(f'{k}: {v}')
-                break
+                                            if target_software != '*':
+                                                cpe['target_software'] = target_software
 
-with open('data/ghsa/package_info.json', 'w+') as f:
-    json.dump(found, f, indent=2)
+                                            package_metadata['cpe_mapping'][cpe_key] = cpe
+                                            package_metadata['cpe_configurations'].append(cpe)
 
-for ecosystem, package_dict in found.items():
-    if ecosystem == 'pip':
-        ecosystem = 'pypi'
+            updated_package_metadata[lookup_key] = package_metadata
 
-    os.makedirs(f'output/{ecosystem}', exist_ok=True)
+#with open('output/ghsa/package_info_debug.json', 'w+') as f:
+#    json.dump(updated_package_metadata, f, indent=2)
 
-    for name, infos in package_dict.items():
-        cpe_configs = []
+updates = []
 
-        for info in infos:
-            cpe_config = {}
+for lookup_key, package_metadata in updated_package_metadata.items():
+    del package_metadata['cpe_mapping']
 
-            if info.get('part') and info['part'] != 'a':
-                cpe_config['part'] = info['part']
+    if lookup_key in current_package_metadata:
+        if package_metadata != current_package_metadata[lookup_key]:
+            updates.append(package_metadata)
+    elif len(package_metadata['cpe_configurations']) > 0:
+        updates.append(package_metadata)
 
-            cpe_config['vendor'] = info['vendor']
-            cpe_config['product'] = info['product']
-        
-            if info.get('target_software') and info['target_software'] != '*':
-                cpe_config['target_software'] = info['target_software']
-            
-            cpe_configs.append(cpe_config)
+for package_metadata in updates:
+    ecosystem = package_metadata['ecosystem']
+    name = package_metadata['name']
+    outdir = f'output/all/{ecosystem}'
 
-        package_config_entry = {
-            'name': name,
-            'ecosystem': ecosystem,
-            'cpe_configurations': cpe_configs
-        }
+    if severity:
+        outdir = f'output/{severity}/{ecosystem}'
 
-        filename = os.path.basename(name)
+    os.makedirs(outdir, exist_ok=True)
 
-        if name == filename:
-            toml_path = f'output/{ecosystem}/{filename}.toml'
-        else:
-            directory = os.path.dirname(name)
-            toml_path = f'output/{ecosystem}/{directory}/{filename}.toml'
+    filename = os.path.basename(name)
 
-            os.makedirs(f'output/{ecosystem}/{directory}', exist_ok=True)
+    if name == filename:
+        toml_path = f'{outdir}/{filename}.toml'
+    else:
+        directory = os.path.dirname(name)
+        toml_path = f'{outdir}/{directory}/{filename}.toml'
 
-        with open(toml_path, 'w') as f:
-            toml.dump(package_config_entry, f)
+        os.makedirs(f'{outdir}/{directory}', exist_ok=True)
+
+    with open(toml_path, 'w') as f:
+        toml.dump(package_metadata, f)
 
